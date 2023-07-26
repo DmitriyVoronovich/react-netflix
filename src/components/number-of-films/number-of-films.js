797c6f0760aa108e95741c093b586f66615cf25b
Dmitriy Vorornovich <dmitriy.vr20@gmail.com>
2023-07-26 18:04:30 +0300

    Revert "start netflix project"
    This reverts commit f5e54bc2c262826cad772359c93c751b5efdd1bd.

--- src/components/number-of-films/number-of-films.js
+++
@@ -1,9 +0,0 @@
-import './number_of_films.css'
-
-function NumberOfFilms () {
-    return (
-        <div className={'number_of_films'}><span className={'number'}>39</span> movies found</div>
-    )
-}
-
-export default NumberOfFilms;
