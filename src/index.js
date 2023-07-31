43290ea977e86ad28c187bc60bf2ee9e7e26e3e5
Dmitriy Vorornovich <dmitriy.vr20@gmail.com>
2023-07-31 13:59:23 +0300

    add <ErrorBoundary/>

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import {App} from 'components';
 import reportWebVitals from './reportWebVitals';
+import ErrorBoundary from "./components/error-boundary";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <App/>
+        <ErrorBoundary fallback={<p>Something went wrong</p>}>
+            <App/>
+        </ErrorBoundary>
     </React.StrictMode>
 );
 
